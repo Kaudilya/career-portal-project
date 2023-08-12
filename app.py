f69f44a7686da20c2cdb23825faba016a4f108c3
Kaudilya <137365036+Kaudilya@users.noreply.github.com>
2023-08-12 04:25:12 +0000

    Added HTML and CSS for the website

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-  return "Hello People of Atlanta!!"
+  return render_template('home.html')
 
 if __name__ == "__main__":
-  # print("Inside if")
   app.run(host='0.0.0.0', debug=True)
